9c8aa24e9a453e2074d2470e0971f4da4a968a2e
AlisonGlenn <118784338+AlisonGlenn@users.noreply.github.com>
2024-03-03 17:00:48 -0700

    blazor practice

---
+++ HowToDoBlazor/HowToDoBlazorSystem/ViewModels/GroceryListView.cs
@@ -0,0 +1,14 @@
+﻿namespace HowToDoBlazorSystem.ViewModels
+{
+    public class GroceryListView
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public decimal MinDiscount { get; set; }
+        public decimal MaxDiscount { get; set; }
+
+    }
+}

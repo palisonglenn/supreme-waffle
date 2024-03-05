e1a5c0a02a68aaaf653ecef7f35d8c9c4c758b02
thaile27 <haile27@hotmail.ca>
2024-03-05 15:45:53 -0700

    initial commit, includes the BlazorSearchList startup file

---
+++ BlazorSearchList/BlazorSearchList/Components/Pages/Samples/CategorySearchList.razor.cs
@@ -0,0 +1,60 @@
+﻿using BlazorSearchListSystem.BLL;
+using BlazorSearchListSystem.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorSearchList.Components.Pages.Samples
+{
+    public partial class CategorySearchList
+    {
+        #region Properties
+        [Inject]
+        protected CategoryService CategoryService { get; set; }
+        #endregion
+
+        #region Fields
+        private List<GroceryListView> CategoryList = new();
+        private string feedback = "";
+        private string name = "Hai";
+
+
+        #endregion
+
+        #region Methods
+        private void ShowFeedback()
+        {
+            feedback = "C# sucks";
+        }
+
+        private void HideFeedback()
+        {
+            feedback = "";
+        }
+        private void GetGroceryList()
+        {
+            try
+            {
+                CategoryList = CategoryService.GetGroceryList();
+            }
+            #region catch all exceptions
+            catch (AggregateException ex)
+            {
+                foreach (var error in ex.InnerExceptions)
+                {
+                    feedback = error.Message;
+                }
+            }
+
+            catch (ArgumentNullException ex)
+            {
+                feedback = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+
+            catch (Exception ex)
+            {
+                feedback = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            #endregion
+        }
+        #endregion
+    }
+}
---
+++ BlazorSearchList/BlazorSearchListSystem/BLL/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using BlazorSearchListSystem.DAL;
+using BlazorSearchListSystem.ViewModels;
+
+namespace BlazorSearchListSystem.BLL
+{
+    public class CategoryService
+    {
+        #region Fields
+        private readonly GroceryListContext? _groceryListContext;
+        #endregion
+
+        #region Constructors
+        internal CategoryService(GroceryListContext GroceryListContext)
+        {
+            _groceryListContext = GroceryListContext;
+        }
+        #endregion
+
+        public List<GroceryListView> GetGroceryList()
+        {
+            return _groceryListContext.Categories
+                .Select(x => new GroceryListView
+                {
+                    ID = x.CategoryID,
+                    Name = x.Description,
+                    Count = x.Products.Count(),
+                    MinPrice = x.Products.Min(x => x.Price),
+                    MaxPrice = x.Products.Max(x => x.Price),
+                    MinDiscount = x.Products.Where(x => x.Discount > 0)
+                                .Min(x => x.Discount),
+                    MaxDiscount = x.Products.Max(x => x.Discount)
+
+                })
+                    .OrderBy(x => x.Name)
+                    .ToList();
+        }
+    }
+}
